d9b47f0bdb5831a01e70d1e32b6b81a1adb5be18
Suryo Prasetyo W <the.oyrus@gmail.com>
2023-02-22 14:13:59 +0700

    Naik Versi 1.0.1 Set Android: - set compileSdkVersion 33 - permission camera stream & location
    Library:
    - flutter_filter_dialog
    - flutter_map, flutter_map_location_marker
    - geocoding, latlong2, location, trust_location
    - glass
    - flutter_swipe_action_cell

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <geolocator_windows/geolocator_windows.h>
 #include <sentry_flutter/sentry_flutter_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  GeolocatorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("GeolocatorWindows"));
   SentryFlutterPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SentryFlutterPlugin"));
 }
